69ead25c79b0b90aea40b3852242f1912246381e
seyoung <seyoung0916@naver.com>
2025-10-31 00:27:24 +0900

    web deploy

--- streamlit.sh
+++ streamlit.sh
@@ -1,2 +1,6 @@
-pip install streamlit
+#!/bin/bash
+set -e
+cd /home/site/wwwroot
+python -m pip install --upgrade pip
+pip install -r requirements.txt
 python -m streamlit run pressm_dashboard.py --server.port 8000 --server.address 0.0.0.0
